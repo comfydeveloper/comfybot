60831ed7a04217946bb985d827ded04dd89c131e
comfydev <comfydeveloper@gmail.com>
2020-01-03 21:53:40 +0100

    Initial commit

---
+++ Source/ComfyBot.Application/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ComfyBot.Application
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    [ExcludeFromCodeCoverage]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = SetupConfiguration();
+
+            IServiceCollection services = new ServiceCollection();
+            services.Add(new ServiceDescriptor(typeof(IConfiguration), provider => configuration, ServiceLifetime.Singleton));
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            Console.ReadLine();
+        }
+
+        private static IConfiguration SetupConfiguration()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                                           .AddJsonFile("appsettings.json", true, true)
+                                           .Build();
+
+            return configuration;
+        }
+    }
+}
