5f7c162da32a1c90f9e8ffd32f91483ef7679c63
comfydev <comfydeveloper@gmail.com>
2020-12-12 21:25:01 +0100

    Add data storage for redeem responses

---
+++ Source/ComfyBot.Data/Models/RewardRedeemResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace ComfyBot.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RewardRedeemResponse : Entity
+    {
+        public string RewardTitle { get; set; }
+
+        public List<string> Replies { get; set; }
+
+        public DateTime? LastUsed { get; set; }
+
+        public int UseCount { get; set; }
+
+        public int TimeoutInSeconds { get; set; }
+    }
+}
---
+++ Source/ComfyBot.Data/Repositories/RewardRedeemResponseRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace ComfyBot.Data.Repositories
+{
+    using System;
+
+    using ComfyBot.Data.Database;
+    using ComfyBot.Data.Models;
+
+    public class RewardRedeemResponseRepository : Repository<RewardRedeemResponse>
+    {
+        public RewardRedeemResponseRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory, "rewardRedeemResponses")
+        { }
+
+        protected override void Update(RewardRedeemResponse source, RewardRedeemResponse target)
+        {
+            Clear(target);
+            UpdateInternal(source, target);
+        }
+
+        private static void Clear(RewardRedeemResponse target)
+        {
+            target.Replies.Clear();
+        }
+
+        private static void UpdateInternal(RewardRedeemResponse source, RewardRedeemResponse target)
+        {
+            target.RewardTitle = source.RewardTitle;
+            target.Replies.AddRange(source.Replies);
+            target.TimeoutInSeconds = source.TimeoutInSeconds;
+            target.UseCount = Math.Max(source.UseCount, target.UseCount);
+            target.LastUsed = source.LastUsed ?? target.LastUsed;
+        }
+    }
+}
